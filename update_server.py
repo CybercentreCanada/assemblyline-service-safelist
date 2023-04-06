22eeee942c7de7b9b4012bf15faa07595b3408e1
cccs-rs <ryan.samaroo@cyber.gc.ca>
2023-04-06 16:12:03 +0000

    Optimize for filtered searches

--- update_server.py
+++ update_server.py
@@ -156,16 +156,14 @@ def extract_safelist(file, pattern, logger, safe_distributors_list=[]):
                 with sqlite3.connect(safelist_file) as db:
                     # Include expected header for CSV format
                     csv.write("SHA-256,SHA-1,MD5,Filename,Filesize\n")
-                    for r in db.execute("SELECT FILE.sha256, FILE.sha1, FILE.md5, FILE.file_name, FILE.file_size, FILE.package_id FROM FILE"):
-                        file_info, pkg_id = r[:-1], r[-1]
-                        if not safe_distributors_list_regex:
-                            # We want to add every file hash
-                            csv.write(','.join([str(i).strip() for i in file_info]) + "\n")
-                        else:
-                            mfg = [r[0] for r in db.execute(f"SELECT MFG.name FROM PKG JOIN MFG USING (manufacturer_id) WHERE PKG.package_id='{pkg_id}'")][0]
-                            if re.match(safe_distributors_list_regex, mfg):
-                                # We want to be selective of which file hash to add
-                                csv.write(','.join([str(i).strip() for i in file_info]) + "\n")
+                    package_filter = ""
+                    if safe_distributors_list_regex:
+                        # Retrieve the list of package_ids associated to each manufacturer
+                        logger.info(f'Retrieving package_ids that belong to the following distributor pattern: {safe_distributors_list_regex}')
+                        package_ids = [str(r[1]) for r in db.execute("SELECT MFG.name, PKG.package_id FROM PKG JOIN MFG USING (manufacturer_id)") if re.match(pattern, r[0])]
+                        package_filter = f"WHERE FILE.package_id IN ({', '.join(package_ids)})"
+                    for r in db.execute(f"SELECT FILE.sha256, FILE.sha1, FILE.md5, FILE.file_name, FILE.file_size FROM FILE {package_filter}"):
+                        csv.write(','.join([str(i).strip() for i in r]) + "\n")
                 csv.flush()
                 os.unlink(safelist_file)
                 safelist_file = csv.name
